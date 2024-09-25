82b5055d8bafb26ed963ead67314a7b1c2115dd8
metinbicaksiz <metinbicaksiz@gmail.com>
2024-09-26 00:29:34 +0300

    new files system

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-import Body from "./components/Body";
 
 function App() {
   return (
-    <Body />
+    <div className="text-3xl text-center text-blue-600 m-10 font-bold">
+      <h1>main page</h1>
+    </div>
   );
 }
 

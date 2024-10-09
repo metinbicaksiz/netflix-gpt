6186a8cdacda1803e4a188fe40c9e439055146f4
metinbicaksiz <metinbicaksiz@gmail.com>
2024-10-09 10:52:24 +0300

    firebase auth issue fixed

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -1,3 +1,4 @@
+import {getAuth} from 'firebase/auth';
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
@@ -15,3 +16,4 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+export const auth = getAuth();

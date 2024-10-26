32f3de29b6d30a3594e09f652d68d155bcf6fc30
Metin Bicaksiz <72347095+metinbicaksiz@users.noreply.github.com>
2024-10-26 19:45:27 +0300

    Update GptRecommendations.jsx

--- src/components/GptRecommendations.jsx
+++ src/components/GptRecommendations.jsx
@@ -1,8 +1,20 @@
 import React from 'react';
+import {useSelector} from "react-redux";
+import MovieList from "./MovieList";
 
 const GptRecommendations = () => {
+    const { movieNames, movieResults } = useSelector(state => state.gpt);
+    if (!movieNames) return null;
+
     return (
-        <div>
+        <div className="p-4 m-10 bg-black bg-opacity-80 text-white rounded-lg">
+            <div>
+                {
+                    movieResults.map((movie, index) => (
+                        <MovieList key={movie.title} title={movieNames[index]} movies={movieResults[index]} />
+                    ))
+                }
+            </div>
         </div>
     );
 };

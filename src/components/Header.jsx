385097c7533bef77772fb0b8f58f8db53b4fbbd6
Metin Bicaksiz <72347095+metinbicaksiz@users.noreply.github.com>
2024-10-26 20:03:33 +0300

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ import React, {useEffect} from 'react';
 import logo from '../assets/logo.png';
 import { auth } from "../utils/firebase";
 import {onAuthStateChanged, signOut} from "firebase/auth";
-import {useNavigate} from "react-router-dom";
+import {Link, useNavigate} from "react-router-dom";
 import {useDispatch, useSelector} from "react-redux";
 import {addUser, removeUser} from "../utils/userSlice";
 import {toggleGptSearchView} from "../utils/gptSlice";
@@ -53,7 +53,9 @@ const Header = () => {
 
     return (
         <div className="absolute w-screen px-8 py-2 bg-gradient-to-b from-black z-10 flex justify-between">
-            <img src={logo} className="w-44" alt="logo"/>
+            <Link to={"/"}>
+                <img src={logo} className="w-44" alt="logo"/>
+            </Link>
             {
                 user &&
                 (<div className="flex p-2 items-center">
